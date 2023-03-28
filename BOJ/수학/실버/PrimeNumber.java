e3c626333dfadbbe423fc66920f1883d66681798
Geonhee Yoon <80369805+geon-hee-Yoon@users.noreply.github.com>
2023-03-28 21:39:46 +0900

    Update and rename Main.java to PrimeNumber.java
    수학 알고리즘/실버5/1978 소수찾기

--- BOJ/수학/실버/PrimeNumber.java
+++ BOJ/수학/실버/PrimeNumber.java
@@ -3,7 +3,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main{
+public class PrimeNumber{
     public static void main(String[] args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         int n=Integer.parseInt(br.readLine());
