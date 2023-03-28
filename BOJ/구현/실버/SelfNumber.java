913109ffebf017f04430f69517a0ef6d1f8d111d
Geonhee Yoon <80369805+geon-hee-Yoon@users.noreply.github.com>
2023-03-28 21:33:22 +0900

    Create SelfNumber.java
    구현 알고리즘/실버5/셀프넘버

---
+++ BOJ/구현/실버/SelfNumber.java
@@ -0,0 +1,28 @@
+//구현/실버 5/4673 셀프넘버
+public class SelfNumber {
+    public static void main(String[] args) {
+        boolean[] chk = new boolean[10001];
+
+        for(int i=1;i<=10001;i++){
+            int n=d(i);
+
+            if(n<10001) chk[n]=true;
+        }
+        StringBuilder sb=new StringBuilder();
+
+        for(int i=1;i<=10000;i++) {
+            if (!chk[i]){
+                sb.append(i).append("\n");
+            }
+        }System.out.println(sb);
+    }
+
+    public static int d(int n){
+        int sum=n;
+
+        while(n!=0){
+            sum+=n%10;
+            n=n/10;
+        }return sum;
+    }
+}
