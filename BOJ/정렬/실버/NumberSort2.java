ae2eaf74ac26a728fbca1788354ed7406a3895fc
Geonhee Yoon <80369805+geon-hee-Yoon@users.noreply.github.com>
2023-04-06 21:27:40 +0900

    Create NumberSort2.java
    정렬/실버5/2751 수 정렬하기2

---
+++ BOJ/정렬/실버/NumberSort2.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class NumberSort2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) list.add(Integer.parseInt(br.readLine()));
+        br.close();
+        Collections.sort(list);
+
+        for (int i = 0; i < n; i++) bw.write(list.get(i)+"\n");
+        bw.flush();
+        bw.close();
+    }
+}
