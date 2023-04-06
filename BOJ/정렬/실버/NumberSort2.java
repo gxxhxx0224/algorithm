4f9e13ab1db9b36927a02bb3d1b9432dd508b401
Geonhee Yoon <80369805+geon-hee-Yoon@users.noreply.github.com>
2023-04-06 21:33:24 +0900

    Add files via upload
    정렬/실버5/2751 수 정렬하기2(BufferedReader와 StringBuilder 이용)

--- BOJ/정렬/실버/NumberSort2.java
+++ BOJ/정렬/실버/NumberSort2.java
@@ -1,11 +1,13 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class NumberSort2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
 
         ArrayList<Integer> list = new ArrayList<>();
@@ -14,8 +16,8 @@ public class NumberSort2 {
         br.close();
         Collections.sort(list);
 
-        for (int i = 0; i < n; i++) bw.write(list.get(i)+"\n");
-        bw.flush();
-        bw.close();
+        for (int value : list) sb.append(value).append("\n");
+
+        System.out.print(sb);
     }
 }
