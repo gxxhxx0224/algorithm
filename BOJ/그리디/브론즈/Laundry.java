55cb61e329ae32e84b6e67d1fae15aaf3f293609
Geonhee Yoon <80369805+geon-hee-Yoon@users.noreply.github.com>
2023-04-18 09:26:19 +0900

    그리디 알고리즘/브론즈3/2720 세탁소 사장 동혁
    그리디 알고리즘/브론즈3/2720 세탁소 사장 동혁

---
+++ BOJ/그리디/브론즈/Laundry.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//그리디 알고리즘
+public class Laundry {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int t=Integer.parseInt(br.readLine()); //테스트의 개수
+
+        for(int i=0;i<t;i++){
+            int c=Integer.parseInt(br.readLine()); //거스름돈 액수
+            int coin[]=new int[4]; //각각의 동전이 담긴 배열
+            //쿼터:$0.25,다임:$0.10,니켈:$0.05,페니:$0.01 순
+
+            while(c!=0){
+                if(c>=25){
+                    c-=25;
+                    coin[0]++;
+                }
+                else if(c>=10){
+                    c-=10;
+                    coin[1]++;
+                }
+                else if(c>=5){
+                    c-=5;
+                    coin[2]++;
+                }
+                else if(c>=1){
+                    c-=1;
+                    coin[3]++;
+                }
+            }
+            System.out.println(coin[0]+" "+coin[1]+" "+coin[2]+" "+coin[3]);
+        }br.close();
+    }
+}
