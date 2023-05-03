2af2c811ab3a2ee25e311796b081893784140061
Geonhee Yoon <80369805+geon-hee-Yoon@users.noreply.github.com>
2023-05-03 22:07:42 +0900

    심화2/브론즈 1/1037 약수
    심화2/브론즈 1/1037 약수

---
+++ BOJ/심화2/divisor.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class divisor {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+        int divisor[] = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        br.close();
+
+        int min=Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for(int i=0;i<n;i++){
+            divisor[i] = Integer.parseInt(st.nextToken());
+            min = Math.min(min, divisor[i]);
+            max = Math.max(max, divisor[i]);
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(min * max);
+        System.out.print(sb);
+
+    }
+}
