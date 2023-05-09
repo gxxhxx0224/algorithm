88b09d8e6e73487fdc3832eebbe8d63d6d6b0b7e
Geonhee Yoon <80369805+geon-hee-Yoon@users.noreply.github.com>
2023-05-09 23:42:06 +0900

    Create snail.java 수학/브론즈 1/2869 달팽이는 올라가고 싶다
    수학/브론즈 1/2869 달팽이는 올라가고 싶다

---
+++ BOJ/수학/브론즈/snail.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class snail {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+
+        int up=Integer.parseInt(st.nextToken());
+        int down=Integer.parseInt(st.nextToken());
+        int length=Integer.parseInt(st.nextToken());
+
+        int day = (length - down) / (up - down);
+
+        if( (length-down)%(up-down)!=0) day++;
+
+        System.out.print(day);
+
+
+    }
+}
