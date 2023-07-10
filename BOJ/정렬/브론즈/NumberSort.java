8e98f1fd2dea3702a74cd7ebafeff840fdd04411
Geonhee Yoon <80369805+geon-hee-Yoon@users.noreply.github.com>
2023-07-10 23:36:00 +0900

    정렬/브론즈 2/2750 수 정렬하기
    정렬/브론즈 2/2750 수 정렬하기

--- BOJ/정렬/브론즈/NumberSort.java
+++ BOJ/정렬/브론즈/NumberSort.java
@@ -6,10 +6,10 @@ public class NumberSort {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         int n=Integer.parseInt(br.readLine());  //수의 개수
         int[]arr=new int[n];
-        for(int i=0;i<n;i++) arr[i]=Integer.parseInt(br.readLine());
+        for(int i=0;i<n;i++) arr[i]=Integer.parseInt(br.readLine()); //수 입력
         br.close();
 
-        Arrays.sort(arr);
+        Arrays.sort(arr); //정렬
 
         for (int i = 0; i < n; i++) System.out.println(arr[i]);
     }
