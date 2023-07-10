5100160772cb2c3da6ba142b2df45347f192b960
Geonhee Yoon <80369805+geon-hee-Yoon@users.noreply.github.com>
2023-07-10 23:37:31 +0900

    정렬/브론즈 2/2587 대표값2
    정렬/브론즈 2/2587 대표값2

--- BOJ/정렬/브론즈/RepresentNumber2.java
+++ BOJ/정렬/브론즈/RepresentNumber2.java
@@ -3,15 +3,15 @@ import java.io.*;
 public class RepresentNumber2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        int[]arr=new int[5];
+        int[]arr=new int[5]; //배열 생성
         int sum=0;
         for(int i=0;i<5;i++) {
             arr[i]=Integer.parseInt(br.readLine());
-            sum+=arr[i];
+            sum+=arr[i]; //합
         }br.close();
         System.out.println(sum/5);
 
-        for(int i=0;i<4;i++){
+        for(int i=0;i<4;i++){ //비교 후 정렬
             for(int j=i+1;j<5;j++){
                 if(arr[j]<arr[i]){
                     int prev=arr[i];
