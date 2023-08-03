a6eac97d4053e8401eac5318086ccc4483ea594d
Geonhee Yoon <80369805+geon-hee-Yoon@users.noreply.github.com>
2023-08-03 23:48:16 +0900

    자료구조/큐/실버 4/2164 카드2
    자료구조/큐/실버 4/2164 카드2(Deque_LinkedList를 이용한 구현)

---
+++ BOJ/자료구조/큐/실버/Card2_Deque_LinkedList.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+
+//Deque LinkedList를 이용한 구현
+public class Main{
+    public static int size=0;
+    public static int front=0;
+    public static int back=0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Deque<Integer> q = new LinkedList<>();
+        br.close();
+
+        for(int i=1;i<=n;i++) q.offer(i);
+
+        while(q.size()>1){
+            q.poll(); //제일 위에 있는 카드를 버린다
+
+            //제일 위에 있는 카드를 제일 아래 있는 카드 밑으로 옮긴다.
+            int temp=q.poll();
+            q.offer(temp);
+        }
+        System.out.print((q.poll()));
+    }
+}
