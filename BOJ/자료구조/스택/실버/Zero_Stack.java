9577f69e5fafe3eec53f8b6257493ca79672f4d8
Geonhee Yoon <80369805+geon-hee-Yoon@users.noreply.github.com>
2023-08-21 20:52:13 +0900

    자료구조/스택/실버 4/10773 제로(스택 클래스를 이용한 구현)
    자료구조/스택/실버 4/10773 제로(스택 클래스를 이용한 구현)

---
+++ BOJ/자료구조/스택/실버/Zero_Stack.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+//스택 클래스를 이용한 구현
+public class Zero_Stack {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int k=Integer.parseInt(br.readLine());
+        Stack<Integer>stack=new Stack<>();
+
+        for(int i=0;i<k;i++){
+            int n=Integer.parseInt(br.readLine());
+
+            if(n!=0){
+                stack.push(n);
+            }
+            else{
+                stack.pop();
+            }
+        }
+        int sum=0;
+
+        while(!stack.isEmpty()) sum+=stack.pop();
+
+        System.out.print(sum);
+    }
+}
