eeae3b39f988766a52480f7b33c8fe14c0308e0c
Geonhee Yoon <80369805+geon-hee-Yoon@users.noreply.github.com>
2023-10-09 21:47:54 +0900

    그래프와 순회/실버 2/24479 알고리즘 수업-깊이우선탐색1
    그래프와 순회/실버 2/24479 알고리즘 수업-깊이우선탐색1

---
+++ BOJ/그래프와 순회/DFS1.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class DFS1 {
+    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+    static int check[];
+    static int count;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int vertex=Integer.parseInt(st.nextToken()); //정점의 수
+        int edge=Integer.parseInt(st.nextToken()); //간선의 수
+        int startVertex=Integer.parseInt(st.nextToken()); //시작정점
+
+        check = new int[vertex + 1];
+
+        for(int i=0;i<vertex+1;i++){
+            graph.add(new ArrayList<>());
+        }
+
+        for(int i=0;i<edge;i++){
+            st=new StringTokenizer(br.readLine());
+            int from=Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+
+            graph.get(from).add(to);
+            graph.get(to).add(from);
+        }
+        count=1;
+
+        for(int i=1;i<graph.size();i++){
+            Collections.sort(graph.get(i));
+        }
+        StringBuilder sb = new StringBuilder();
+        dfs(startVertex);
+
+        for(int i=1;i<check.length;i++){
+            sb.append(check[i]).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    public static void dfs(int vertex){
+        check[vertex]=count;
+
+        for(int i=0;i<graph.get(vertex).size();i++){
+            int newVertex=graph.get(vertex).get(i);
+
+            if(check[newVertex]==0){
+                count++;
+                dfs(newVertex);
+            }
+        }
+    }
+}
