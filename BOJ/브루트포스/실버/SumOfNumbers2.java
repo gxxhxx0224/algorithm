ab1a4154f36a40410cd910d8a6c361180817ddcf
geon-hee-Yoon <80369805+gxxhxx0224@users.noreply.github.com>
2023-12-14 21:12:49 +0900

    브루트포스/실버 4/2003 수들의 합2
    브루트포스/실버 4/2003 수들의 합2

---
+++ BOJ/브루트포스/실버/SumOfNumbers2.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class SumOfNumbers2{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n=Integer.parseInt(st.nextToken());
+        int m=Integer.parseInt(st.nextToken());
+
+        int arr[]=new int[n];
+
+        st=new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++)
+            arr[i]=Integer.parseInt(st.nextToken());
+
+        int count=0;
+
+        for(int i=0;i<n;i++){
+            int sum=0;
+
+            for(int j=i;j<n;j++){
+                sum+=arr[j];
+
+                if(sum==m){
+                    count++;
+                    break;
+                }
+                else if(sum>m)
+                    break;
+            }
+        }
+        System.out.print(count);
+    }
+}
