f01477293fd4d1cce7d4ea8fe00ae8c298d8a104
geon-hee-Yoon <80369805+gxxhxx0224@users.noreply.github.com>
2024-01-01 23:52:11 +0900

    그리디/실버 4/1049 기타줄
    그리디/실버 4/1049 기타줄

--- BOJ/그리디/실버/Guitar.java
+++ BOJ/그리디/실버/Guitar.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class guitar {
+public class Guitar {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
