8ee05ca05f699b208cbc46e100bc39890826cfa4
geon-hee-Yoon <80369805+gxxhxx0224@users.noreply.github.com>
2024-01-19 23:34:32 +0900

    동적 프로그래밍/실버 3/1463 1로만들기
    동적 프로그래밍/실버 3/1463 1로만들기

---
+++ BOJ/동적 프로그래밍/실버/make1.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class make1 {
+    static int dp[];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine()); //정수 N
+        dp=new int[n+1]; //각 인덱스에 해당되는 수의 연산횟수 배열
+        dp[0]=dp[1]=0; //1은 0회
+
+        for(int i=2;i<=n;i++){
+            recur(i);
+        }
+        System.out.print(dp[n]);
+    }
+    public static void recur(int num){
+        if(num%6==0){
+            dp[num]=Math.min(dp[num/3],Math.min(dp[num/2],dp[num-1]))+1;
+        }
+        else if(num%3==0){
+            dp[num]=Math.min(dp[num/3],dp[num-1])+1;
+        }
+        else if(num%2==0){
+            dp[num]=Math.min(dp[num/2],dp[num-1])+1;
+        }
+        else{
+            dp[num]=dp[num-1]+1;
+        }
+    }
+}
