fbf82a9d8f55edc0ac62a7dc19635d0c1e29b60c
geon-hee-Yoon <80369805+gxxhxx0224@users.noreply.github.com>
2024-02-05 23:58:59 +0900

    동적 프로그래밍/실버 3/1463 1로만들기(재귀)
    동적 프로그래밍/실버 3/1463 1로만들기(재귀)

--- BOJ/동적 프로그래밍/실버/make1_TopDown.java
+++ BOJ/동적 프로그래밍/실버/make1_TopDown.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class make1 {
+public class make1_TopDown {
     static int dp[];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
