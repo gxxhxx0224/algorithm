a974d363121b5766dfe9b2e73b0c34385a7e4b45
geon-hee-Yoon <80369805+gxxhxx0224@users.noreply.github.com>
2024-03-26 23:05:28 +0900

    백트래킹/실버 3/15649 N과M(1)
    백트래킹/실버 3/15649 N과M(1)

---
+++ BOJ/백트래킹/NandM_1.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class NandM_1 {
+    static int arr[];
+    static boolean check[];
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n =Integer.parseInt(st.nextToken());
+        int m =Integer.parseInt(st.nextToken());
+        arr=new int[m];
+        check=new boolean[n +1];
+
+        sb=new StringBuilder();
+
+        dfs(n,m,0);
+        System.out.print(sb);
+    }
+    public static void dfs(int n,int m,int depth){
+        if(depth==m){
+            for(int val:arr)
+                sb.append(val+" ");
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=1;i<=n;i++){
+            if(!check[i]){
+                check[i]=true;
+                arr[depth]=i;
+                dfs(n,m,depth+1);
+                check[i]=false;
+            }
+        }
+        return;
+    }
+}
