cd5a41b3ab086b87b2220fdc671cb22740562c08
geon-hee-Yoon <80369805+gxxhxx0224@users.noreply.github.com>
2024-04-02 23:15:32 +0900

    백트래킹/실버 3/15654 N과M 5
    백트래킹/실버 3/15654 N과M 5

---
+++ BOJ/백트래킹/NansM_5.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class NansM_5 {
+    static int n,m, num[],arr[];
+    static boolean check[];
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n=Integer.parseInt(st.nextToken());
+        m=Integer.parseInt(st.nextToken());
+        arr=new int[m];
+        num=new int[n+1];
+        check=new boolean[n+1];
+
+        st=new StringTokenizer(br.readLine());
+        for(int i=1;i<=n;i++)
+            num[i]=Integer.parseInt(st.nextToken());
+        Arrays.sort(num);
+
+        sb=new StringBuilder();
+
+        dfs(0);
+        System.out.print(sb);
+    }
+    public static void dfs(int depth){
+        if(depth==m){
+            for(int val:arr){
+                sb.append(val+" ");
+            }sb.append("\n");
+            return;
+        }
+
+        for(int i=1;i<=n;i++){
+            if(!check[i]){
+                check[i]=true;
+                arr[depth]=num[i];
+                dfs(depth+1);
+                check[i]=false;
+            }
+        }
+    }
+}
