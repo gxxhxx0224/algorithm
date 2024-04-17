09158d584a8c7b53a3389c4b27aa73507759b1eb
geon-hee-Yoon <80369805+gxxhxx0224@users.noreply.github.com>
2024-04-18 00:06:38 +0900

    DP/브론즈 1/11050 이항계수 1(Bottom Up)
    DP/브론즈 1/11050 이항계수 1(Bottom Up)

--- BOJ/동적 프로그래밍/브론즈/BinomialCoefficient_1_BottomUp.java
+++ BOJ/동적 프로그래밍/브론즈/BinomialCoefficient_1_BottomUp.java
@@ -3,8 +3,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-//dp
-public class BinomialCoefficient_1 {
+//dp bottom-up
+public class BinomialCoefficient_1_BottomUp {
     static int n,k,dp[][];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
