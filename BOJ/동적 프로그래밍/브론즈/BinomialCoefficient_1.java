6727c659148444e0e764a4ed3153a689dc876adb
geon-hee-Yoon <80369805+gxxhxx0224@users.noreply.github.com>
2024-04-17 22:19:06 +0900

    DP/브론즈 1/이항 계수 1
    DP/브론즈 1/이항 계수 1

---
+++ BOJ/동적 프로그래밍/브론즈/BinomialCoefficient_1.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//dp
+public class BinomialCoefficient_1 {
+    static int n,k,dp[][];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n=Integer.parseInt(st.nextToken());
+        k=Integer.parseInt(st.nextToken());
+        dp=new int[11][11];
+
+        for(int i=0;i<=10;i++){
+            dp[i][0]=1;
+            dp[i][i]=1;
+        }
+
+        for(int i=2;i<=10;i++){
+            for(int j=1;j<=10;j++){
+                dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+            }
+        }
+        System.out.println(dp[n][k]);
+    }
+
+}
