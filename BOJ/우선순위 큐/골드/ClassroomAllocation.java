8ebb276f08234488c779af2f956b1331a80127f4
geon-hee-Yoon <80369805+gxxhxx0224@users.noreply.github.com>
2024-08-28 21:26:56 +0900

    PriorityQueue/Gold 5/11000 강의실 배정
    PriorityQueue/Gold 5/11000 강의실 배정

---
+++ BOJ/우선순위 큐/골드/ClassroomAllocation.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class ClassroomAllocation {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int time[][] = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            time[i][0] = Integer.parseInt(st.nextToken());
+            time[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(time, (o1, o2) -> {
+            if (o1[0] == o2[0]) return o1[1] - o2[1];
+            else return o1[0] - o2[0];
+        });
+
+        PriorityQueue<Integer>pq=new PriorityQueue<>();
+
+        for(int i=0;i<n;i++){
+            if(!pq.isEmpty()&& pq.peek()<=time[i][0])
+                pq.poll();
+            pq.offer(time[i][1]);
+        }
+        System.out.println(pq.size());
+    }
+}
