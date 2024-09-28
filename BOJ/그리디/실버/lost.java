53a610ffa61750686b135b808dfc0e3a9d128ed2
geon-hee-Yoon <80369805+gxxhxx0224@users.noreply.github.com>
2024-09-28 20:04:47 +0900

    Silver 2/1541 잃어버린 괄호
    Silver 2/1541 잃어버린 괄호

---
+++ BOJ/그리디/실버/lost.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        String s = "";
+
+        int ans = 0;
+        boolean minus = false;
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+
+            if (Character.isDigit(c)) {
+                s += c;
+            } else {
+                if (!s.isEmpty()) {
+                    if (minus) {
+                        ans -= Integer.parseInt(s);
+                        s="";
+                    } else {
+                        ans += Integer.parseInt(s);
+                        s="";
+                    }
+                }
+                if (c == '-') minus = true;
+            }
+        }
+        if(!s.isEmpty()){
+            if(minus)ans-=Integer.parseInt(s);
+            else ans+=Integer.parseInt(s);
+        }
+        System.out.println(ans);
+    }
+}
