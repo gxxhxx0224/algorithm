7556ae141feb8ebfff2f3270b32ee56c4e830533
geon-hee-Yoon <80369805+gxxhxx0224@users.noreply.github.com>
2024-12-15 21:15:22 +0900

    Bronze 2/1550 16진수
    Bronze 2/1550 16진수

---
+++ BOJ/구현/브론즈/Hexadecimal.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // 입력 받기
+        String hexInput = scanner.nextLine();
+
+        // 16진수를 10진수로 변환
+        int decimalOutput = Integer.parseInt(hexInput, 16);
+
+        // 결과 출력
+        System.out.println(decimalOutput);
+
+        scanner.close();
+    }
+}
