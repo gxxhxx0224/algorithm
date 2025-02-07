cf3f78e797d3a84002e41f13db7621aa9b2c547f
geon-hee-Yoon <80369805+gxxhxx0224@users.noreply.github.com>
2025-02-07 17:25:36 +0900

    Gold 2/12738 "가장 긴 증가하는 부분 수열 3"
    Gold 2/12738 "가장 긴 증가하는 부분 수열 3"

---
+++ BOJ/이분탐색/골드/LIS_3.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+
+//G2 12015 "가장 긴 증가하는 부분 수열2" LIS 3
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+        int a[]=new int[n+1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=1;i<=n;i++)
+            a[i]=Integer.parseInt(st.nextToken());
+
+        int length=1;
+        int list[]=new int[n+1];
+        list[1]=a[1];
+
+        for(int i=2;i<=n;i++){
+            int num=a[i];
+
+            if(list[length]<num){
+                list[++length]=num;
+            }else{
+                int low=1;
+                int high=length;
+
+                while(low<high){
+                    int mid=(low+high)/2;
+
+                    if(list[mid]<num) low=mid+1;
+                    else high=mid;
+                }
+                list[low]=num;
+            }
+        }
+        System.out.print(length);
+    }
+}
