457374dd32d1f657a60582e0c6085c14e0cb0af8
geon-hee-Yoon <80369805+gxxhxx0224@users.noreply.github.com>
2025-03-06 23:22:06 +0900

    Bronze 4 / 11282 한글
    Bronze 4 / 11282 한글

---
+++ BOJ/구현/브론즈/Korean.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+
+        char result = (char) ('가' + N - 1);
+        System.out.println(result);
+    }
+}
