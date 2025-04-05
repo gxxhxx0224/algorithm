1ee1eb6044696ed335eec0d67fcc62ed4590e8ed
geon-hee-Yoon <80369805+gxxhxx0224@users.noreply.github.com>
2025-04-05 21:05:24 +0900

    C.java 만들기

---
+++ BOJ/수학/브론즈/C.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();  // Number of test cases
+
+        for (int i = 0; i < t; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            System.out.println(x + y);
+        }
+
+        sc.close();
+    }
+}
