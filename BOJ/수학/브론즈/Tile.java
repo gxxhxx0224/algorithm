579594b8cfa343e998605dc5be274ea8fd61466e
geon-hee-Yoon <80369805+gxxhxx0224@users.noreply.github.com>
2025-04-29 20:55:13 +0900

    Create Tile.java

---
+++ BOJ/수학/브론즈/Tile.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long N = Long.parseLong(st.nextToken());
+        long M = Long.parseLong(st.nextToken());
+
+        System.out.println((N * M) / 2);
+    }
+}
