7016aceaf192f127e5c490d51d28eb9aff081e7e
geon-hee-Yoon <80369805+gxxhxx0224@users.noreply.github.com>
2025-08-02 22:24:49 +0900

    Hour.java 만들기

---
+++ BOJ/수학/브론즈/Hour.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        System.out.println(X * 24);
+    }
+}
