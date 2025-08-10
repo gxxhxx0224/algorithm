37eb2ac5389997d4fbac0748753f7d7e525a96df
geon-hee-Yoon <80369805+gxxhxx0224@users.noreply.github.com>
2025-08-10 22:11:43 +0900

    1325 효율적인해킹_dfs(리스트)
    1325 효율적인해킹_dfs(리스트)

---
+++ BOJ/그래프와 순회/실버/EfficientHacking_DFS_List.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n,m,count[];
+    static List<List<Integer>>list=new ArrayList<>();
+    static boolean check[];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n=Integer.parseInt(st.nextToken());
+        m=Integer.parseInt(st.nextToken());
+        count=new int[n+1];
+
+        for(int i=0;i<=n;i++)
+            list.add(new ArrayList<>());
+
+        for(int i=0;i<m;i++){
+            st=new StringTokenizer(br.readLine());
+            int to=Integer.parseInt(st.nextToken());
+            int from=Integer.parseInt(st.nextToken());
+
+            list.get(from).add(to);
+        }
+
+        for(int i=1;i<=n;i++) {
+            check=new boolean[n+1];
+            check[i]=true;
+            dfs(i, i);
+        }
+
+        int max=0;
+        for(int i=1;i<=n;i++)
+            max=Math.max(max,count[i]);
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=1;i<=n;i++){
+            if(max==count[i])
+                sb.append(i).append(" ");
+        }
+        System.out.print(sb);
+    }
+
+    public static void dfs(int now,int start){ //시간초과
+        for(int next:list.get(now)){
+            if(!check[next]) {
+                check[next]=true;
+                count[start]++;
+                dfs(next, start);
+            }
+        }
+    }
+}
