c974278a9859ce61f9a7f4588b69b19f1be495ed
geon-hee-Yoon <80369805+gxxhxx0224@users.noreply.github.com>
2025-08-10 21:44:40 +0900

    5567 결혼식_BFS(리스트)/S2
    5567 결혼식_BFS(리스트)/S2

---
+++ BOJ/그래프와 순회/실버/WeddingCeremony_BFS_List.java
@@ -0,0 +1,68 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n, m;
+    static List<List<Integer>> list = new ArrayList<>();
+    static boolean check[];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine()); //동기의 수
+        m = Integer.parseInt(br.readLine()); //리스트 길이
+        check = new boolean[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+
+            list.get(from).add(to);
+            list.get(to).add(from);
+        }
+
+        bfs(1, 1);
+
+        int ans = -1; //1:자신은 포함하지 않으므로 -1부터
+        for (boolean t : check) {
+            if (t)
+                ans++;
+        }
+
+        System.out.print(ans == -1 ? 0 : ans);
+    }
+
+    public static void bfs(int depth, int start) {
+        Queue<int[]> q = new ArrayDeque<>();
+        int now = start;
+        q.add(new int[]{depth, now});
+        check[now] = true;
+
+        while (!q.isEmpty()) {
+            depth = q.peek()[0];
+            now = q.peek()[1];
+            q.poll();
+
+            if (depth < 3) {
+                for (int next : list.get(now)) {
+                    q.add(new int[]{depth+1,next});
+                    check[next]=true;
+                }
+            }
+        }
+    }
+
+    public static void dfs(int depth, int now) {
+        if (depth >= 3)
+            return;
+
+        for (int next : list.get(now)) {
+            check[next] = true;
+            dfs(depth + 1, next);
+        }
+    }
+}
